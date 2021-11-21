4a3a3069d62c8c8ab1625aeb84e7a1d25b028938
George Chen <monstercoder@gmail.com>
2021-11-21 14:11:34 -0600

    fix cors

---
+++ _api/Program.cs
@@ -0,0 +1,21 @@
+using SquidGame.Api.Hubs;
+const string FRONTEND = "frontend";
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(Options => {
+    Options.AddPolicy(FRONTEND, builder => {
+        builder.WithOrigins("http://localhost:1234") 
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        ;
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+app.UseRouting();
+app.UseCors(FRONTEND);
+app.UseEndpoints(endpoints =>endpoints.MapHub<GameHub>("/gameHub"));
+app.Run();
